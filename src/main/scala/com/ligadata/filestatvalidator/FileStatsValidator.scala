036e542c8ac3d37129a57943d9ac2de3787a69d8
ligadata-haitham <haitham@ligadata.com>
2016-12-07 18:03:24 +0200

    change in logic to have much less hits to Hive

--- src/main/scala/com/ligadata/filestatvalidator/FileStatsValidator.scala
+++ src/main/scala/com/ligadata/filestatvalidator/FileStatsValidator.scala
@@ -2,7 +2,7 @@ package com.ligadata.filestatvalidator
 
 import java.io._
 import java.nio.file.{Files, Paths}
-import java.sql.{Connection, DriverManager, ResultSet, Statement}
+import java.sql.{Connection, DriverManager, ResultSet, SQLSyntaxErrorException, Statement}
 import java.util
 import java.util.Properties
 
@@ -14,7 +14,6 @@ import org.json.JSONObject
 
 import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
-import scala.util.control.Breaks._
 
 object FileStatsValidator {
 
@@ -44,7 +43,7 @@ object FileStatsValidator {
 
 
   //  def validateFileStats(fileStatsTableName: String, successEventsTableName: String, failedEventsTableName: String, partitionFieldName: String, partitionDate: String, conn: Connection): ArrayBuffer[(String, String)] = {
-  def validateFileStats(fileStatsTableName: String, fileStatsTablePartitionFiledName: String, fileStatsTablePartitionDate: String, fileStatsTablePartitionStartHour: String, fileStatsTablePartitionEndHour: String, successEventsTablePartitionFiledName: String, successEventsTablePartitionValue: String, failedEventsTableName: String, failedEventsTablePartitionFiledName: String, failedEventsTablePartitionValue: String, feedsToFileNamesMappingLocation: String, conn: Connection): ArrayBuffer[(String, String)] = {
+  def validateFileStats(fileStatsTableName: String, fileStatsTablePartitionFiledName: String, fileStatsTablePartitionDate: String, fileStatsTablePartitionStartHour: String, fileStatsTablePartitionEndHour: String, successEventsTablesNames: String, successEventsTablePartitionFiledName: String, successEventsTablePartitionValue: String, failedEventsTableName: String, failedEventsTablePartitionFiledName: String, failedEventsTablePartitionValue: String, feedsToFileNamesMappingLocation: String, conn: Connection): ArrayBuffer[(String, String)] = {
 
     var finalResult: ArrayBuffer[(String, String)] = new ArrayBuffer[(String, String)]
     var fileNamesAndRecordCounts: ArrayBuffer[(String, Double)] = new ArrayBuffer[(String, Double)]
@@ -53,8 +52,7 @@ object FileStatsValidator {
 
     //Step 1 : get all unique file names and recordscount for given date partition in table ch11_test.file_stats
     var st: Statement = conn.createStatement()
-    var whereStatement: String = " where ( " + fileStatsTablePartitionFiledName + "='" + fileStatsTablePartitionDate + "' AND recordscount>0 )"
-    //+ " AND hour>=" + fileStatsTablePartitionStartHour + " AND hour<=" + fileStatsTablePartitionEndHour
+    var whereStatement: String = " where ( " + fileStatsTablePartitionFiledName + "='" + fileStatsTablePartitionDate + "' AND recordscount>0 )" + " AND hour>=" + fileStatsTablePartitionStartHour + " AND hour<=" + fileStatsTablePartitionEndHour
     val query1: String = "Select distinct(filename), recordscount from " + fileStatsTableName + whereStatement
     try {
       val rs1: ResultSet = st.executeQuery(query1)
@@ -62,80 +60,120 @@ object FileStatsValidator {
         fileNamesAndRecordCounts += ((rs1.getString(1), rs1.getDouble(2)))
       }
     } catch {
-      case e: Exception => {
+      case e: SQLSyntaxErrorException => {
         logger.error("FileStatValidator : error running statement: " + query1, e)
       }
     }
 
+    //Step 2 : get all unique file names and recordscount for given date partition in all outputTables;
+    val successTablesNamesList: Array[String] = successEventsTablesNames.split(",")
+    var successEventsFilesAndCounts: mutable.HashMap[String, Double] = new mutable.HashMap[String, Double]
 
-    //Step 2 : get a hashmap of FilePath and SuccessTableName mapping
-    var filePathToTableNameMap: mutable.HashMap[String, String] = jsonToHashMap(feedsToFileNamesMappingLocation)
-
-    logger.debug("FileStatValidator : Looping on All files retrieved from " + fileStatsTableName)
-    fileNamesAndRecordCounts.foreach(oneFileStats => {
-      val fullFileName = oneFileStats._1
-      val fileName = fullFileName.substring(fullFileName.lastIndexOf("/"), fullFileName.length)
-      val filePath = fullFileName.substring(0, fullFileName.lastIndexOf("/"))
-      val recordsCount = oneFileStats._2
-
-      //Step 3 : get correct SuccessTableName
-      var successEventsTableName: String = ""
-      breakable {
-        filePathToTableNameMap.foreach(x => {
-          if (x._2.contains(filePath)) {
-            successEventsTableName = x._1
-            break
-          }
-        })
-      }
-      logger.debug("FileStatValidator : successEventsTableName is: " + successEventsTableName)
-
-      var successEventsCount: Double = -1
-      var failedEventsCount: Double = -1
-      var resultCountValidation: String = ""
-      var failurePercentage: String = ""
-
-      logger.debug("FileStatValidator : Getting the number of records for file " + fileName + " from table " + successEventsTableName)
-      // Step 4 : get the number of records from the SuccessEventsTable
-      val query2 = "Select count(*) from " + successEventsTableName + " where " + successEventsTablePartitionFiledName + "=" + successEventsTablePartitionValue + " and filename=" + fileName
+    successTablesNamesList.foreach(successEventTableName => {
+      //   select file_name, count(*) from kprod.SandvineReconciliation where date_loaded='2016-12-06' group by file_name
+      var whereStatement2: String = " where " + successEventsTablePartitionFiledName + "=" + successEventsTablePartitionValue
+      val query2: String = "select file_name, count(*) from " + successEventTableName + whereStatement2 + " group by file_name"
 
       try {
         val rs2: ResultSet = st.executeQuery(query2)
         while (rs2.next()) {
-          successEventsCount = rs2.getDouble(1)
+          successEventsFilesAndCounts.put(rs2.getString(1), rs2.getDouble(2))
         }
       } catch {
-        case e: Exception => {
+        case e: SQLSyntaxErrorException => {
           logger.error("FileStatValidator : error running statement: " + query2, e)
         }
       }
-      logger.debug("FileStatValidator : Getting the number of records for file " + fileName + " from table " + failedEventsTableName)
-      // Step5 : get the number of records from the FailedEventsTable
-      val query3 = "Select count(*) from " + failedEventsTableName + " where " + failedEventsTablePartitionFiledName + "=" + failedEventsTablePartitionValue + " and filename=" + fileName
-
-      try {
-        val rs3: ResultSet = st.executeQuery(query3)
-        while (rs3.next()) {
-          failedEventsCount = rs3.getDouble(1)
-        }
-      } catch {
-        case e: Exception => {
-          logger.error("FileStatValidator : error running statement: " + query3, e)
-        }
-      }
-      logger.debug("FileStatValidator : Validating records count")
-      if (recordsCount == (successEventsCount + failedEventsCount)) {
-        resultCountValidation = "count matches for file " + fileName
-      } else {
-        resultCountValidation = "count Does NOT matche for file " + fileName + ", file_stats count : %1.0f , SuccessEventsTable count : %1.0f , FailedEventsTable count : %1.2f , difference is : %1.0f".format(
-          recordsCount, successEventsCount, failedEventsCount, recordsCount - successEventsCount - failedEventsCount)
-      }
-
-      logger.debug("FileStatValidator : Calculating failure percentage")
-      failurePercentage = calculateFailurePercentage(successEventsCount, failedEventsCount)
-      finalResult += ((resultCountValidation, failurePercentage))
-
     })
+
+
+    //Step 3 : get all unique file names and recordscount for given date partition in failedEventsTable;
+    var failedEventsFilesAndCounts: mutable.HashMap[String, Double] = new mutable.HashMap[String, Double]
+
+    //    Select filename, count(*) from kprod.rejecteddata where rejectiondate='20161206' group by filename
+    var whereStatement3: String = " where " + failedEventsTablePartitionFiledName + "=" + failedEventsTablePartitionValue
+    val query3: String = "Select filename, count(*) from " + failedEventsTableName + whereStatement3 + " group by filename"
+
+    try {
+      val rs3: ResultSet = st.executeQuery(query3)
+      while (rs3.next()) {
+        failedEventsFilesAndCounts.put(rs3.getString(1), rs3.getDouble(2))
+      }
+    } catch {
+      case e: SQLSyntaxErrorException => {
+        logger.error("FileStatValidator : error running statement: " + query3, e)
+      }
+    }
+
+
+    //    //Step 2 : get a hashmap of FilePath and SuccessTableName mapping
+    //    var filePathToTableNameMap: mutable.HashMap[String, String] = jsonToHashMap(feedsToFileNamesMappingLocation)
+    //
+    //    logger.debug("FileStatValidator : Looping on All files retrieved from " + fileStatsTableName)
+    //    fileNamesAndRecordCounts.foreach(oneFileStats => {
+    //      val fullFileName = oneFileStats._1
+    //      val fileName = fullFileName.substring(fullFileName.lastIndexOf("/"), fullFileName.length)
+    //      val filePath = fullFileName.substring(0, fullFileName.lastIndexOf("/"))
+    //      val recordsCount = oneFileStats._2
+    //
+    //      //Step 3 : get correct SuccessTableName
+    //      var successEventsTableName: String = ""
+    //      breakable {
+    //        filePathToTableNameMap.foreach(x => {
+    //          if (x._2.contains(filePath)) {
+    //            successEventsTableName = x._1
+    //            break
+    //          }
+    //        })
+    //      }
+    //      logger.debug("FileStatValidator : successEventsTableName is: " + successEventsTableName)
+    //
+    //      var successEventsCount: Double = -1
+    //      var failedEventsCount: Double = -1
+    //      var resultCountValidation: String = ""
+    //      var failurePercentage: String = ""
+    //
+    //      logger.debug("FileStatValidator : Getting the number of records for file " + fileName + " from table " + successEventsTableName)
+    //      // Step 4 : get the number of records from the SuccessEventsTable
+    //      val query2 = "Select count(*) from " + successEventsTableName + " where " + successEventsTablePartitionFiledName + "=" + successEventsTablePartitionValue + " and filename=" + fileName
+    //
+    //      try {
+    //        val rs2: ResultSet = st.executeQuery(query2)
+    //        while (rs2.next()) {
+    //          successEventsCount = rs2.getDouble(1)
+    //        }
+    //      } catch {
+    //        case e: Exception => {
+    //          logger.error("FileStatValidator : error running statement: " + query2, e)
+    //        }
+    //      }
+    //      logger.debug("FileStatValidator : Getting the number of records for file " + fileName + " from table " + failedEventsTableName)
+    //      // Step5 : get the number of records from the FailedEventsTable
+    //      val query3 = "Select count(*) from " + failedEventsTableName + " where " + failedEventsTablePartitionFiledName + "=" + failedEventsTablePartitionValue + " and filename=" + fileName
+    //
+    //      try {
+    //        val rs3: ResultSet = st.executeQuery(query3)
+    //        while (rs3.next()) {
+    //          failedEventsCount = rs3.getDouble(1)
+    //        }
+    //      } catch {
+    //        case e: Exception => {
+    //          logger.error("FileStatValidator : error running statement: " + query3, e)
+    //        }
+    //      }
+    //      logger.debug("FileStatValidator : Validating records count")
+    //      if (recordsCount == (successEventsCount + failedEventsCount)) {
+    //        resultCountValidation = "count matches for file " + fileName
+    //      } else {
+    //        resultCountValidation = "count Does NOT matche for file " + fileName + ", file_stats count : %1.0f , SuccessEventsTable count : %1.0f , FailedEventsTable count : %1.2f , difference is : %1.0f".format(
+    //          recordsCount, successEventsCount, failedEventsCount, recordsCount - successEventsCount - failedEventsCount)
+    //      }
+    //
+    //      logger.debug("FileStatValidator : Calculating failure percentage")
+    //      failurePercentage = calculateFailurePercentage(successEventsCount, failedEventsCount)
+    //      finalResult += ((resultCountValidation, failurePercentage))
+    //
+    //    })
 
     finalResult
   }
@@ -191,6 +229,7 @@ object FileStatsValidator {
     var fileStatsTablePartitionStartHour: String = ""
     var fileStatsTablePartitionEndHour: String = ""
 
+    var successEventsTablesNames: String = ""
     var successEventsTablePartitionValue: String = ""
     var successEventsTablePartitionFiledName: String = ""
 
@@ -213,6 +252,7 @@ object FileStatsValidator {
       // load a properties file
       prop.load(input)
       // get the property value and print it out
+      successEventsTablesNames = prop.getProperty("success.events.tables.names")
       successEventsTablePartitionValue = prop.getProperty("success.events.table.partition.value")
       successEventsTablePartitionFiledName = prop.getProperty("success.events.table.partition.field.name")
 
@@ -235,6 +275,7 @@ object FileStatsValidator {
       println("fileStatsTablePartitionDate: " + fileStatsTablePartitionDate)
       println("fileStatsTablePartitionStartHour: " + fileStatsTablePartitionStartHour)
       println("fileStatsTablePartitionEndHour: " + fileStatsTablePartitionEndHour)
+      println("successEventsTablesNames: " + successEventsTablesNames)
       println("successEventsTablePartitionValue: " + successEventsTablePartitionValue)
       println("successEventsTablePartitionFiledName: " + successEventsTablePartitionFiledName)
       println("failedEventsTableName: " + failedEventsTableName)
@@ -246,7 +287,7 @@ object FileStatsValidator {
 
 
       connection = getConnection(hiveConf)
-      val result1 = validateFileStats(fileStatsTableName, fileStatsTablePartitionFiledName, fileStatsTablePartitionDate, fileStatsTablePartitionStartHour, fileStatsTablePartitionEndHour, successEventsTablePartitionFiledName, successEventsTablePartitionValue, failedEventsTableName, failedEventsTablePartitionFiledName, failedEventsTablePartitionValue, feedsToFileNamesMappingLocation, connection)
+      val result1 = validateFileStats(fileStatsTableName, fileStatsTablePartitionFiledName, fileStatsTablePartitionDate, fileStatsTablePartitionStartHour, fileStatsTablePartitionEndHour, successEventsTablesNames, successEventsTablePartitionFiledName, successEventsTablePartitionValue, failedEventsTableName, failedEventsTablePartitionFiledName, failedEventsTablePartitionValue, feedsToFileNamesMappingLocation, connection)
 
       result1.foreach(singleFileResult => {
         println("Records Count Validation Result: " + singleFileResult._1)
