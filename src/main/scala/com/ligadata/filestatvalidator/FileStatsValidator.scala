fce749cd00563f6ba74cbab72ab811b2252746ad
ligadata-haitham <haitham@ligadata.com>
2016-12-08 15:30:01 +0200

    simple select count(*) query

--- src/main/scala/com/ligadata/filestatvalidator/FileStatsValidator.scala
+++ src/main/scala/com/ligadata/filestatvalidator/FileStatsValidator.scala
@@ -67,11 +67,11 @@ object FileStatsValidator {
 
     //    var whereStatement: String = " where ( " + fileStatsTablePartitionFiledName + "='" + fileStatsTablePartitionDate + "' AND A.recordscount>0 AND `hour` >=" + fileStatsTablePartitionStartHour + " AND `hour` <=" + fileStatsTablePartitionEndHour + ")"
     //    val query1: String = "Select distinct(filename), recordscount from " + fileStatsTableName + whereStatement
-    val query1: String = "select * from ch11_test.file_stats limit ?"
+    val query1: String = "select count(*) from ch11_test.file_stats"
     println(">>>>>>>>>>>>>>>>>>>>" + query1)
     try {
       val st1: PreparedStatement = conn.prepareStatement(query1)
-      st1.setDouble(1, 1)
+      //      st1.setDouble(1, 1)
       st1.execute()
       val rs1: ResultSet = st1.getGeneratedKeys
 
